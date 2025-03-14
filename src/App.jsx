eba4a66011aa9dc313e900bfd517e8b44f57b1ba
Jonathan-GZX <jonathan.melgar002@gmail.com>
2025-03-14 11:10:14 -0600

    enrutamiento

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import Dimencion1 from './pages/D1_page'
+import Dimencion2 from './pages/D2_page'
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/d1" element={<Dimencion1 />} />
+        <Route path="/d2" element={<Dimencion2 />} />
       </Routes>
     </div>
   )
