eb1caab6731d17f498114a654849278d1fd6d75d
Jonathan-GZX <jonathan.melgar002@gmail.com>
2025-03-14 11:19:36 -0600

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import Dimencion1 from './pages/D1_page'
 import Dimencion2 from './pages/D2_page'
+import Dimencion3 from './pages/D3_page'
+import Dimencion4 from './pages/D4_page'
+import Dimencion5 from './pages/D5_page'
+import Dimencion6 from './pages/D6_page'
+import Dimencion7 from './pages/D7_page'
+import config from './pages/ConfigPage'
 
 
 function App() {
@@ -12,6 +18,12 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/d1" element={<Dimencion1 />} />
         <Route path="/d2" element={<Dimencion2 />} />
+        <Route path="/d3" element={<Dimencion3 />} />
+        <Route path="/d4" element={<Dimencion4 />} />
+        <Route path="/d5" element={<Dimencion5 />} />
+        <Route path="/d6" element={<Dimencion6 />} />
+        <Route path="/d7" element={<Dimencion7 />} />
+        <Route path="/config" element={<config />} />
       </Routes>
     </div>
   )
