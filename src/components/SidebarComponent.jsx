9a2e7dd1816b31fcbec74c426206a9c2c874168b
Jonathan-GZX <jonathan.melgar002@gmail.com>
2025-03-15 21:36:23 -0600

    Reapply "Mejoras"
    This reverts commit 8f4a859c13a9a95acf687d33b6132c8ddd2ea2c1.

--- src/components/SidebarComponent.jsx
+++ src/components/SidebarComponent.jsx
@@ -1,76 +1,131 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import user from "../icons/ivan.png";
-import dashboard from "../icons/dashboard.svg";
-import chart from "../icons/chart.svg";
-import dolar from "../icons/dolar.svg";
-import help from "../icons/help.svg";
-import music from "../icons/music.svg";
+import Home from "../icons/Home.svg";
+import Dashboard from "../icons/dashboard.svg";
+import task from "../icons/task.svg";
+import users from "../icons/users.svg";
+import search from "../icons/search.svg";
 import settings from "../icons/settings.svg";
 import "../styles/SidebarStyle.css";
 
 function SidebarComponent({ isSidebarOpen }) {
+  // Estados para controlar las listas desplegables
+  const [isTasksDropdownOpen, setTasksDropdownOpen] = useState(false);
+  const [isUsersDropdownOpen, setUsersDropdownOpen] = useState(false);
+
+  // Referencias para detectar clics fuera de las listas desplegables
+  const tasksDropdownRef = useRef(null);
+  const usersDropdownRef = useRef(null);
+
+  // Funciones para alternar las listas desplegables
+  const toggleTasksDropdown = () => {
+    setTasksDropdownOpen(!isTasksDropdownOpen);
+  };
+
+  const toggleUsersDropdown = () => {
+    setUsersDropdownOpen(!isUsersDropdownOpen);
+  };
+
+  // Cerrar las listas desplegables al hacer clic fuera de ellas
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        tasksDropdownRef.current &&
+        !tasksDropdownRef.current.contains(event.target)
+      ) {
+        setTasksDropdownOpen(false);
+      }
+      if (
+        usersDropdownRef.current &&
+        !usersDropdownRef.current.contains(event.target)
+      ) {
+        setUsersDropdownOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className={`SidebarComponent ${isSidebarOpen ? "open" : "closed"}`}>
-      {/* SidebarHeader */}
-      <div className="SidebarHeader">
-        <img src={user} alt="user-icon" />
-        <div className="UserData">
-          <span>TU NOMBRE</span>
-          <span>IVAN</span>
-        </div>
-      </div>
-
       {/* SidebarMenu */}
       <div className="SidebarMenu">
         <ul>
           <li>
+            <Link to="d4" className="Link">
+              <img src={search} alt="lupa-icon" />
+              <span>Buscar</span>
+            </Link>
+          </li>
+          <li>
             <Link to="/" className="Link">
-              <img src={dashboard} alt="inicio-icon" />
-              <span>Inicio</span>
+              <img src={Home} alt="inicio-icon" />
+              <span>Home</span>
             </Link>
           </li>
           <li>
             <Link to="/d1" className="Link">
-              <img src={chart} alt="D1-icon" />
-              <span>Dimencion 1</span>
+              <img src={Dashboard} alt="D1-icon" />
+              <span>Dashboard</span>
             </Link>
           </li>
-          <li>
-            <Link to="/d2" className="Link">
-              <img src={dolar} alt="D2-icon" />
-              <span>Dimencion 2</span>
-            </Link>
+
+          {/* Tareas con lista desplegable */}
+          <li ref={tasksDropdownRef} onClick={toggleTasksDropdown}>
+            <div className="Link">
+              <img src={task} alt="D2-icon" />
+              <span>Tareas</span>
+            </div>
+            {isTasksDropdownOpen && (
+              <ul className="DropdownMenu">
+                <li>
+                  <Link to="/tasks/all" className="DropdownLink">
+                    All Task
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/tasks/in-progress" className="DropdownLink">
+                    In Progress
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/tasks/done" className="DropdownLink">
+                    Done
+                  </Link>
+                </li>
+              </ul>
+            )}
           </li>
-          <li>
-            <Link to="/d3" className="Link">
-              <img src={help} alt="D3-icon" />
-              <span>Dimencion 3</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/d4" className="Link">
-              <img src={music} alt="D4-icon" />
-              <span>Dimencion 4</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/d5" className="Link">
-              <img src={music} alt="D5-icon" />
-              <span>Dimencion 5</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/d6" className="Link">
-              <img src={music} alt="D6-icon" />
-              <span>Dimencion 6</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/d7" className="Link">
-              <img src={music} alt="D7-icon" />
-              <span>Dimencion 7</span>
-            </Link>
+
+          {/* Usuarios con lista desplegable */}
+          <li ref={usersDropdownRef} onClick={toggleUsersDropdown}>
+            <div className="Link">
+              <img src={users} alt="D3-icon" />
+              <span>Usuarios</span>
+            </div>
+            {isUsersDropdownOpen && (
+              <ul className="DropdownMenu">
+                <li>
+                  <Link to="/users/profile" className="DropdownLink">
+                    Mi Perfil
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/users/create" className="DropdownLink">
+                    Crear Nuevo Usuario
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/users/roles" className="DropdownLink">
+                    Roles y Permisos
+                  </Link>
+                </li>
+              </ul>
+            )}
           </li>
         </ul>
       </div>
