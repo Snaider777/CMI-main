73b9f1fab8ee63d6633788bcce3da20190086d8b
Jonathan-GZX <jonathan.melgar002@gmail.com>
2025-03-15 21:35:56 -0600

    Revert "Merge branch 'Jonathan' of https://github.com/Snaider777/CMI-main into Jonathan"
    This reverts commit a63ef923888786d65205e19004754bbccae071ad, reversing
    changes made to d9cb2df10c5044cf005d424854c356cd038726f1.

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,16 @@ import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import Dimencion1 from './pages/D1_page'
 import Dimencion2 from './pages/D2_page'
-import LoginComponent from './components/LoginComponent'
+
 
 function App() {
   return (
     <div className="Home">
-        <Routes>
-          <Route path="/login" element={<LoginComponent />} />
-          <Route path="/" element={<HomePage />} />
-          <Route path="/d1" element={<Dimencion1 />} />
-          <Route path="/d2" element={<Dimencion2 />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/d1" element={<Dimencion1 />} />
+        <Route path="/d2" element={<Dimencion2 />} />
+      </Routes>
     </div>
   )
 }
