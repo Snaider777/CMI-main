819eb303a9a4c8186f6d338636d9aba2db6e857c
Jonathan-GZX <jonathan.melgar002@gmail.com>
2025-03-16 21:36:54 -0600

    De regreso al sidebar :(
    Bueno quería hacer las animaciones del menú y el sidebar desplegable, pero me costo mucho mas de lo que esperaba. Bueno si lo logre, pero no se aplicaban algunas configuraciones y me desespere. asique lo intentare en otro momento

--- src/components/SidebarComponent.jsx
+++ src/components/SidebarComponent.jsx
@@ -1,114 +1,82 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import '../styles/SidebarStyle.css';
 import 'boxicons/css/boxicons.min.css';
 import { Link } from 'react-router-dom';
-import ModoClaro from '../icons/brand-light.svg';
+import ModoClaro from '../icons/logoUSO.png';
 import user from '../icons/user.png';
 
 function SidebarComponent() {
-  // Estados para controlar la visibilidad de los submenús
-  const [isStoreOpen, setIsStoreOpen] = useState(false);
-  const [isFileOpen, setIsFileOpen] = useState(false);
-
-  // Referencias para los submenús
-  const storeMenuRef = useRef(null);
-  const fileMenuRef = useRef(null);
-
-  // Función para alternar el submenú de Store
-  const toggleStoreMenu = () => {
-    const subMenu = storeMenuRef.current;
-    if (isStoreOpen) {
-      subMenu.style.height = '0px';
-    } else {
-      subMenu.style.height = `${subMenu.scrollHeight}px`;
-    }
-    setIsStoreOpen(!isStoreOpen);
-  };
-
-  // Función para alternar el submenú de File
-  const toggleFileMenu = () => {
-    const subMenu = fileMenuRef.current;
-    if (isFileOpen) {
-      subMenu.style.height = '0px';
-    } else {
-      subMenu.style.height = `${subMenu.scrollHeight}px`;
-    }
-    setIsFileOpen(!isFileOpen);
-  };
-
   return (
-    <div className='SidebarComponent'>
+    <div className="SidebarComponent open">
       <div className="Header">
         <div className="menu-btn">
-          <i className='bx bx-chevron-left'></i>
+          <i className="bx bx-chevron-left"></i>
         </div>
         <div className="brand">
           <img src={ModoClaro} alt="logo" />
-          <span>USO</span>
+          <span>Admin</span>
         </div>
       </div>
 
       <div className="MenuContainer">
         <div className="search">
-          <i className='bx bx-search'></i>
-          <input type="search" placeholder='Search' />
+          <i className="bx bx-search"></i>
+          <input type="search" placeholder="Search" />
         </div>
 
         <ul className="menu">
           <li className="menu-item menu-item-static active">
-            <Link to="/" className='menu-link'>
-              <i className='bx bx-home-alt-2'></i>
+            <Link to="/" className="menu-link">
+              <i className="bx bx-home-alt-2"></i>
               <span>Home</span>
             </Link>
           </li>
 
           <li className="menu-item menu-item-static">
-            <Link to="/Dashboard" className='menu-link'>
-              <i className='bx bx-bar-chart-alt-2'></i>
+            <Link to="/Dashboard" className="menu-link">
+              <i className="bx bx-bar-chart-alt-2"></i>
               <span>Dashboard</span>
             </Link>
           </li>
 
-          {/* Menú desplegable para Store */}
-          <li className={`menu-item menu-item-dropdown ${isStoreOpen ? 'open' : ''}`}>
-            <div className='menu-link' onClick={toggleStoreMenu}>
-              <i className='bx bx-shopping-bag'></i>
-              <span>Store</span>
-              <i className={`bx ${isStoreOpen ? 'bx-chevron-up' : 'bx-chevron-down'}`}></i>
+          <li className="menu-item menu-item-dropdown">
+            <div className="menu-link">
+              <i class='bx bx-user' ></i>
+              <span>Users</span>
+              <i className="bx bx-chevron-down"></i>
             </div>
-            <ul className="sub-menu" ref={storeMenuRef} style={{ height: '0px', overflow: 'hidden', transition: 'height 0.3s ease' }}>
+            <ul className="sub-menu">
               <li>
-                <Link className='Sub-menu-link'>Products</Link>
+                <Link className="Sub-menu-link">My Profile</Link>
               </li>
               <li>
-                <Link className='Sub-menu-link'>Orders</Link>
+                <Link className="Sub-menu-link">Create a new user</Link>
               </li>
               <li>
-                <Link className='Sub-menu-link'>Subscriptions</Link>
+                <Link className="Sub-menu-link">Roles & permission</Link>
               </li>
             </ul>
           </li>
 
           <li className="menu-item menu-item-static">
-            <Link to="/Dashboard" className='menu-link'>
-              <i className='bx bx-task'></i>
-              <span>Task</span>
+            <Link to="/Dashboard" className="menu-link">
+              <i className="bx bx-file-blank"></i>
+              <span>File</span>
             </Link>
           </li>
 
-          {/* Menú desplegable para File */}
-          <li className={`menu-item menu-item-dropdown ${isFileOpen ? 'open' : ''}`}>
-            <div className='menu-link' onClick={toggleFileMenu}>
-              <i className='bx bx-file-blank'></i>
-              <span>File</span>
-              <i className={`bx ${isFileOpen ? 'bx-chevron-up' : 'bx-chevron-down'}`}></i>
+          <li className="menu-item menu-item-dropdown">
+            <div className="menu-link">
+              <i className="bx bx-task"></i>
+              <span>Task</span>
+              <i className="bx bx-chevron-down"></i>
             </div>
-            <ul className="sub-menu" ref={fileMenuRef} style={{ height: '0px', overflow: 'hidden', transition: 'height 0.3s ease' }}>
+            <ul className="sub-menu">
               <li>
-                <Link className='Sub-menu-link'>Images</Link>
+                <Link className="Sub-menu-link">In progress</Link>
               </li>
               <li>
-                <Link className='Sub-menu-link'>Audios</Link>
+                <Link className="Sub-menu-link">Done</Link>
               </li>
             </ul>
           </li>
@@ -118,15 +86,15 @@ function SidebarComponent() {
       <div className="footer">
         <ul className="menu">
           <li className="menu-item menu-item-static">
-            <Link to="/" className='menu-link'>
-              <i className='bx bx-bell'></i>
+            <Link to="/" className="menu-link">
+              <i className="bx bx-bell"></i>
               <span>Notifications</span>
             </Link>
           </li>
 
           <li className="menu-item menu-item-static">
-            <Link to="/Dashboard" className='menu-link'>
-              <i className='bx bx-cog'></i>
+            <Link to="/Dashboard" className="menu-link">
+              <i className="bx bx-cog"></i>
               <span>Setting</span>
             </Link>
           </li>
@@ -142,7 +110,7 @@ function SidebarComponent() {
           </div>
 
           <div className="user-icon">
-            <i className='bx bx-exit'></i>
+            <i className="bx bx-exit"></i>
           </div>
         </div>
       </div>
