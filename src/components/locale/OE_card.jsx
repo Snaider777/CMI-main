0a254d8639c0e9208c568a8e0c96f05b1973457d
CH4P3R <knightchaper777@gmail.com>
2025-03-21 10:38:18 -0600

    BarCardsComponents

--- src/components/locale/OE_card.jsx
+++ src/components/locale/OE_card.jsx
@@ -2,19 +2,22 @@ import { BarChart } from '@mui/x-charts/BarChart';
 import "../../styles/OE_card.css";
 import "../../styles/OE_chart.css";
 
-export default function OE_card({titulo, texto, BAR_CANTIDAD, BAR_DATOS }) {
+export default function OE_card({titulo, texto, color, BAR_CANTIDAD, BAR_DATOS }) {
 
-  const ArregloDatos = [BAR_CANTIDAD];
+  const ArregloCantidad = BAR_CANTIDAD;
+  const ArregloDatos = BAR_DATOS;
 
   return (
     <div className='OE_card'>
         <h1>{titulo}</h1>
         <p>{texto}</p>
+        <div className='line'></div>
         <BarChart
-          xAxis={[{ scaleType: 'band', data: ['Perspectiva 1', 'Perspectiva 2', 'Perspectiva 3', 'Perspectiva 4'] }]}
-          series={[{ data: [30,22,15,21] } ]}
-          width={500}
-          height={300}
+          xAxis={[{ scaleType: 'band', data: ArregloDatos }]}
+          series={[{ data: ArregloCantidad } ]}
+          width={400}
+          height={250}
+          colors={color}
         />
     </div>
   )
--- src/pages/OE_Page.jsx
+++ src/pages/OE_Page.jsx
@@ -5,9 +5,49 @@ import '../styles/OE_page.css'
 function OE_Page() {
   return (
     <div className='OE_Page'>
-      <OE_card titulo='Objetivo Estrategico 1' texto='Description del objetivo estrategico con informacion relevante'/>
-      <OE_card titulo='Objetivo Estrategico 2' texto='Description del objetivo estrategico con informacion relevante'/>
-      <OE_card titulo='Objetivo Estrategico 3' texto='Description del objetivo estrategico con informacion relevante'/>
+      <OE_card 
+        color={['red']}
+        titulo='Objetivo Estrategico 1' 
+        texto='Description del objetivo estrategico con información relevante'
+        BAR_DATOS = {['A', 'B']} 
+        BAR_CANTIDAD = {[1, 3]}
+      />
+      <OE_card 
+        color={['blue']}
+        titulo='Objetivo Estrategico 2' 
+        texto='Description del objetivo estrategico con información relevante'
+        BAR_DATOS = {['A', 'B', 'C']}
+        BAR_CANTIDAD = {[42, 32, 111]}
+      />
+      <OE_card 
+        color={['yellow']}
+        titulo='Objetivo Estrategico 3' 
+        texto='Description del objetivo estrategico con información relevante'
+        BAR_DATOS = {['A', 'B', 'C','D']} 
+        BAR_CANTIDAD = {[42, 322, 150, 99]}
+      />
+      <OE_card 
+        color={['blue']}
+        titulo='Objetivo Estrategico 4' 
+        texto='Description del objetivo estrategico con información relevante'
+        BAR_DATOS = {['A', 'B', 'C']} 
+        BAR_CANTIDAD = {[42, 32, 141]}
+      />
+      <OE_card 
+        color={['gray']}
+        titulo='Objetivo Estrategico 5' 
+        texto='Description del objetivo estrategico con información relevante'
+        BAR_DATOS = {['A', 'B', 'C', 'D', 'E']} 
+        BAR_CANTIDAD = {[42, 32, 11, 55, 46]}
+      />
+      <OE_card 
+        color={['green']}
+        titulo='Objetivo Estrategico 6' 
+        texto='Description del objetivo estrategico con información relevante'
+        BAR_DATOS = {['A', 'B', 'C']} 
+        BAR_CANTIDAD = {[42, 32, 20]}
+      />
+      
     </div>
   )
 }
