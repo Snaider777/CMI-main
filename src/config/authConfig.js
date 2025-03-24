1941ae5e55b217ee07eb04e9b59ea8c2e15f0132
Snaider777 <cristophersnayder0@gmail.com>
2025-03-23 20:38:54 -0600

    completado

--- src/config/authConfig.js
+++ src/config/authConfig.js
@@ -3,7 +3,7 @@ import { PublicClientApplication } from "@azure/msal-browser";
 const msalConfig = {
   auth: {
     clientId: import.meta.env.VITE_AZURE_CLIENT_ID, 
-    authority: "https://login.microsoftonline.com/common",
+    authority: `https://login.microsoftonline.com/${import.meta.env.VITE_AZURE_TENANT_ID}`,
     redirectUri: import.meta.env.VITE_AZURE_REDIRECT_URI,
   },
 };
