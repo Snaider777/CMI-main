66555383339ca01035cee79fc398a27ac4d36dc3
nayels23 <nayelimadaiguz@outlook.es>
2025-03-27 15:24:28 -0600

    cambios card

--- src/components/locale/Cards.jsx
+++ src/components/locale/Cards.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-import { useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
@@ -52,12 +50,12 @@ export default function Cards({ title, value, interval, trend, data }) {
   const trendValues = { up: "+25%", down: "-25%", neutral: "+5%" };
 
   return (
-    <Card variant="outlined" className="CardContent">
+    <Card variant="outlined" sx={{ height: "100%", flexGrow: 1 }}>
       <CardContent>
         <Typography component="h2" variant="subtitle2" gutterBottom>
           {title}
         </Typography>
-        <Stack direction="column" className="StackContent">
+        <Stack direction="column" sx={{ justifyContent: "space-between", flexGrow: "1", gap: 1 }}>
           <Stack sx={{ justifyContent: "space-between" }}>
             <Stack direction="row" sx={{ justifyContent: "space-between", alignItems: "center" }}>
               <Typography variant="h4" component="p">
